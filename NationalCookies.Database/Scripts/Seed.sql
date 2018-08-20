0295a77a7ca5b1e43efa0b35c292e3ff6a0beab7
Barry Luijbregts <b.luijbregts@waardedoorit.nl>
2018-08-20 17:28:09 +0200

    Implemented cache in NationalCookies.MVC

--- NationalCookies.Database/Scripts/Seed.sql
+++ NationalCookies.Database/Scripts/Seed.sql
@@ -2,7 +2,7 @@
 if not exists(select 1 from dbo.cookies) 
 BEGIN
 
-INSERT INTO dbo.cookies (ImageUrl, [Name], Price) values ('https://intcookie.azureedge.net/cdn/cookie-cc.jpg', 'Chololate Chip', 1.2)
+INSERT INTO dbo.cookies (ImageUrl, [Name], Price) values ('https://intcookie.azureedge.net/cdn/cookie-cc.jpg', 'Chocolate Chip', 1.2)
 INSERT INTO dbo.cookies (ImageUrl, [Name], Price) values ('https://intcookie.azureedge.net/cdn/cookie-bc.jpg', 'Butter Cookie', 1.0)
 INSERT INTO dbo.cookies (ImageUrl, [Name], Price) values ('https://intcookie.azureedge.net/cdn/cookie-mc.jpg', 'Macaroons', 0.9)
 
